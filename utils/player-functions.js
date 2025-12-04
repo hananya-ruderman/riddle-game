238e333bd2fc02c7c5cc0e09f9ee4504d0c7fa8b
hananya <chruderman@gmail.com>
2025-12-04 12:07:04 +0200

    player-functions.js added

---
+++ utils/player-functions.js
@@ -0,0 +1,28 @@
+import input from "analiza-sync";
+
+function createPlayer()
+{
+    const userName = input("what is your name")
+    const theUser = {
+        userName: userName,
+        playTime: []
+    } 
+    console.log(theUser)
+    return theUser
+}
+
+function addSolveTime(player, seconds)
+{
+    player.playTime.push(seconds)
+    console.log(player)
+}
+
+function showStats(player)
+{
+    let totalTime, avarageTime;
+    player.playTime.forEach(element => {totalTime += element});
+    avarageTime = totalTime/player.playTime.length
+    console.log(`your total time playing is: ${totalTime}, the avarage for a riddle is: ${avarageTime}`)
+}
+
+showStats(createPlayer())
